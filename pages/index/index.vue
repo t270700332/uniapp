64ce28be71f843c4d4f1b54f6b450318463dfa0c
tianzhili <270700332@qq.com>
2019-10-25 18:06:47 +0800

    增加轮播图

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,9 @@
  <template>
-	<view class="content">
+	<view>
 		<view class="status_bar">
 			<!-- 这里是状态栏 -->
 		</view>
-		<view style="width: 100%;"> 
+		<view style="width:100%;"> 
 	        <view class="index-header">
 	        	<view class="img-view">
 	        		<image src="/static/img/logo.png" mode="" id="user-img"></image>
@@ -13,10 +13,29 @@
 	        	</view>
 	        </view>     	
 		</view>
-		<image class="logo" src="/static/img/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<view class="uni-padding-wrap carousel-wrap">
+            <view class="page-section swiper">
+                <view class="page-section-spacing">
+                    <swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+                        <swiper-item>
+                            <view class="swiper-item">
+                               <image class="swiper-item-img" src="/static/img/index/swiper1.jpg" mode="" ></image> 
+                            </view>
+                        </swiper-item>
+                        <swiper-item>
+                            <view class="swiper-item">
+                                <image class="swiper-item-img" src="/static/img/index/swiper2.jpg" mode="" ></image>
+                            </view>
+                        </swiper-item>
+                        <swiper-item>
+                            <view class="swiper-item">
+                                <image class="swiper-item-img" src="/static/img/index/swiper3.jpg" mode="" ></image>
+                            </view>
+                        </swiper-item>
+                    </swiper>
+                </view>
+            </view>
+        </view>
 	</view>
 </template>
 
@@ -24,7 +43,11 @@
 	export default {
 		data() {
 			return {
-				title: '我的app'
+				title: '我的app',
+                autoplay: true,
+                interval: 2000,
+                indicatorDots: true,
+                duration: 500
 			}
 		},
 		onLoad() {
@@ -48,19 +71,10 @@
 		height: 100%;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+	.carousel-wrap{
+        background-color: #274349;
+    }
 
 	.title {
 		font-size: 36rpx;
@@ -69,18 +83,16 @@
 
 	.index-header {
 		width: 100%;
-		height: 200upx;
+		height: 100upx;
 		background-color: #274349;
-		position: absolute;
-		top: 0;
 	}
 
 	.img-view {
-		width: 90upx;
-		height: 90upx;
-		position: absolute;
-		top: 55px;
-		left: 10px;
+		width: 70upx;
+		height: 70upx;
+        display: inline-block;
+        margin-left: 10px;
+        margin-top: 10px;
 	}
 
 	#user-img {
@@ -90,19 +102,26 @@
 	}
 
 	.user-name-text {
-		position: absolute;
-		left: 70px;
-		top: 70px;
 		width: 100px;
 		overflow: hidden; //超出的文本隐藏
 		text-overflow: ellipsis; //溢出用省略号显示
 		white-space: nowrap; //溢出不换行
 		font-size: 16px;
 		color: #C0C0C0;
+        display: inline-block;
+        position: absolute;
+        top: 60px;
+        left: 60px;
 	}
 
 	.status_bar {
 		height: var(--status-bar-height);
 		width: 100%;
 	}
+    
+    .swiper-item-img{
+        width: 100%;
+        background-size:100% 100%;
+    }
+    
 </style>
