fe2047f6572d73fe5b4d72023cc05e57296c1bb3
tzl <270700332@qq.com>
2019-12-03 21:07:36 +0800

    社区页面修改

---
+++ common/js/sqList.js
@@ -0,0 +1,109 @@
+export default [{
+	"article_type": 0,
+	"image_list": [{
+		"url": "../../static/img/index/wszt.png",
+		"width": 563,
+		"height": 316
+	}, {
+		"url": "../../static/img/index/ecy.png",
+		"width": 641,
+		"height": 360
+	}],
+	"id": "E560F13C-3A2D-4A89-D422-61C309A8E9E9"
+}, {
+	"article_type": 1,
+	"image_list": [{
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 563,
+		"height": 316
+	}, {
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 641,
+		"height": 360
+	}, {
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 640,
+		"height": 360
+	}],
+	"id": "E3263FC6-3719-4B5C-67A9-D3BCA575F8C4"
+}, {
+	"article_type": 2,
+	"image_list": [{
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 563,
+		"height": 316
+	}, {
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 641,
+		"height": 360
+	}, {
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 640,
+		"height": 360
+	}],
+	"id": "878CE254-A6BA-43BE-5EB2-7D62A51FFBA6"
+}, {
+	"article_type": 3,
+	"image_list": [{
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 563,
+		"height": 316
+	}, {
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 641,
+		"height": 360
+	}, {
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 640,
+		"height": 360
+	}],
+	"id": "8F55895A-329A-40C6-B981-D7715AB47E64"
+}, {
+	"article_type": 4,
+	"image_list": [{
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 563,
+		"height": 316
+	}, {
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 641,
+		"height": 360
+	}, {
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 640,
+		"height": 360
+	}],
+	"id": "61B3AB9C-F242-438B-4605-77D55FADF567"
+}, {
+	"article_type": 5,
+	"image_list": [{
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 563,
+		"height": 316
+	}, {
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 641,
+		"height": 360
+	}, {
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 640,
+		"height": 360
+	}],
+	"id": "E17C426C-A219-43D8-6862-DE5C200E0B2B"
+}, {
+	"article_type": 6,
+	"image_list": [{
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 563,
+		"height": 316
+	}],
+	"id": "937F0AA4-AE10-45BE-ABE7-A173DFF05B99"
+}, {
+	"article_type": 7,
+	"image_list": [{
+		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+		"width": 563,
+		"height": 316
+	}],
+	"id": "0A8E4195-947B-4B97-E32D-5EC123D0D063"
+}]
