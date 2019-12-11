872636321caf7daedc00cdbaff29874bcd495a26
tzl <270700332@qq.com>
2019-12-11 23:04:46 +0800

    社区页面修改

--- common/js/sqList.js
+++ common/js/sqList.js
@@ -19,75 +19,99 @@ export default [{
 		"image_list": [{
 			"name": "艾泽拉斯议事厅",
 			"list": [{
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+				"url": "../../static/img/shequ/azls.png",
 				"name": "艾泽拉斯议事厅",
 			}, {
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+				"url": "../../static/img/shequ/fjwyh.png",
 				"name": "艾泽拉斯风纪委员会",
 			}, {
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"url": "../../static/img/shequ/qzzx.png",
 				"name": "前瞻资讯",
 			}, {
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"url": "../../static/img/shequ/jdjs.png",
 				"name": "经典旧世",
 			}, {
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"url": "../../static/img/shequ/gfyw.png",
 				"name": "国服以外",
 			}, {
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"url": "../../static/img/shequ/zbaz.png",
 				"name": "争霸艾泽拉斯",
 			}]
 		}, {
 			"name": "职业讨论区",
 			"list": [{
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-				"name": "艾泽拉斯议事厅",
+				"url": "../../static/img/shequ/wcs.png",
+				"name": "五晨寺",
 			}, {
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-				"name": "艾泽拉斯风纪委员会",
+				"url": "../../static/img/shequ/hfys.png",
+				"name": "黑锋要塞",
 			}, {
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-				"name": "前瞻资讯",
+				"url": "../../static/img/shequ/txsc.png",
+				"name": "铁血沙场",
 			}, {
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-				"name": "经典旧世",
+				"url": "../../static/img/shequ/mfdt.png",
+				"name": "魔法圣堂",
 			}, {
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-				"name": "国服以外",
+				"url": "../../static/img/shequ/xysd.png",
+				"name": "信仰神殿",
 			}, {
-				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-				"name": "争霸艾泽拉斯",
+				"url": "../../static/img/shequ/fbjt.png",
+				"name": "风暴祭坛",
+			}, {
+				"url": "../../static/img/shequ/fcmj.png",
+				"name": "翡翠梦境",
+			}, {
+				"url": "../../static/img/shequ/lsdt.png",
+				"name": "猎手大厅",
+			}, {
+				"url": "../../static/img/shequ/sgzl.png",
+				"name": "圣光之力",
+			}, {
+				"url": "../../static/img/shequ/emsy.png",
+				"name": "恶魔深渊",
+			}, {
+				"url": "../../static/img/shequ/aylk.png",
+				"name": "暗影裂口",
+			}, {
+				"url": "../../static/img/shequ/yldl.png",
+				"name": "伊利达雷",
 			}]
 		}]
 	},
 	{
 		"article_type": 2,
 		"id": "878CE254-A6BA-43BE-5EB2-7D62A51FFBA6",
-		"name": "厂商专区",
+		"name": "网事杂谈",
 		"image_list": [{
-				"name": "职业讨论区",
+				"name": "网事杂谈",
 				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"url": "../../static/img/index/wszt.png",
 					"name": "网事杂谈",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/index/ecy.png",
+					"name": "二次元国家地理",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/jmdc.png",
+					"name": "寂寞的车",
+				}, {
+					"url": "../../static/img/shequ/zcrs.png",
+					"name": "职场人生",
+				}, {
+					"url": "../../static/img/shequ/lq.png",
+					"name": "篮球",
 				}]
 			},
 			{
-				"name": "职业讨论区",
+				"name": "IT软硬件",
 				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/xfdz.png",
+					"name": "消费电子IT新闻",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/yjpz.png",
+					"name": "硬件配置",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/esjy.png",
+					"name": "二手交易",
 				}]
 			}
 		]
@@ -95,31 +119,34 @@ export default [{
 	{
 		"article_type": 3,
 		"id": "8F55895A-329A-40C6-B981-D7715AB47E64",
-		"name": "网事杂谈",
+		"name": "厂商专区",
 		"image_list": [{
-				"name": "职业讨论区",
+				"name": "暴雪游戏",
 				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/swxf.png",
+					"name": "守望先锋",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/lscs.png",
+					"name": "炉石传说",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}]
+					"url": "../../static/img/shequ/ahphs.png",
+					"name": "暗黑破坏神3",
+				}, {
+					"url": "../../static/img/shequ/mszb.png",
+					"name": "魔兽争霸",
+					}]
 			},
 			{
-				"name": "职业讨论区",
+				"name": "拳头游戏",
 				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/yxlm.png",
+					"name": "英雄联盟",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/yxlmss.png",
+					"name": "英雄联盟赛事",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/ydzy.png",
+					"name": "LOL云顶之弈",
 				}]
 			}
 		]
@@ -128,131 +155,72 @@ export default [{
 	{
 		"article_type": 4,
 		"id": "61B3AB9C-F242-438B-4605-77D55FADF567",
-		"name": "网事杂谈",
+		"name": "手机游戏",
 		"image_list": [{
-				"name": "职业讨论区",
+				"name": "手机/网页游戏",
 				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/ppkdc.png",
+					"name": "跑跑卡丁车",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/hpjy.png",
+					"name": "和平精英",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/yys.png",
+					"name": "阴阳师",
 				}]
-			},
-			{
-				"name": "职业讨论区",
-				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}]
-			}
-		]
+			}]
 
 	},
 	{
 		"article_type": 5,
 		"id": "E17C426C-A219-43D8-6862-DE5C200E0B2B",
-		"name": "网事杂谈",
+		"name": "传统游戏",
 		"image_list": [{
-				"name": "职业讨论区",
+				"name": "传统游戏",
 				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/bkm.png",
+					"name": "精灵宝可梦",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/twhj.png",
+					"name": "太吾绘卷",
 				}]
-			},
-			{
-				"name": "职业讨论区",
-				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}]
-			}
-		]
+			}]
 	},
 	{
 		"article_type": 6,
 		"id": "937F0AA4-AE10-45BE-ABE7-A173DFF05B99",
-		"name": "网事杂谈",
+		"name": "网络游戏",
 		"image_list": [{
-				"name": "职业讨论区",
+				"name": "网络游戏",
 				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/tksj.png",
+					"name": "坦克世界",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/my.png",
+					"name": "命运",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/jdqs.png",
+					"name": "绝地求生",
+				}, {
+					"url": "../../static/img/shequ/jw3.png",
+					"name": "剑网3",
 				}]
-			},
-			{
-				"name": "职业讨论区",
-				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}]
-			}
-		]
+			}]
 
 	},
 	{
 		"article_type": 7,
 		"id": "0A8E4195-947B-4B97-E32D-5EC123D0D063",
-		"name": "网事杂谈",
+		"name": "国家地理俱乐部",
 		"image_list": [{
-				"name": "职业讨论区",
+				"name": "国家地理俱乐部",
 				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/yslm.png",
+					"name": "银色黎明裁判所",
 				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
+					"url": "../../static/img/shequ/ltkf.png",
+					"name": "论坛开发",
 				}]
-			},
-			{
-				"name": "职业讨论区",
-				"list": [{
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}, {
-					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-					"name": "网事杂谈",
-				}]
-			}
-		]
+			}]
 	}
 ]
