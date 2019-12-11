8852683d6d74f32b6fee2c2ba363aa31aeea8d47
tzl <270700332@qq.com>
2019-12-11 21:04:40 +0800

    修改社区页样式

--- common/js/sqList.js
+++ common/js/sqList.js
@@ -1,105 +1,258 @@
 export default [{
-	"article_type": 0,
-	"name":"网事杂谈",
-	"image_list": [{
-		"url": "../../static/img/index/wszt.png",
-		"name":"网事杂谈",
+		"article_type": 0,
+		"id": "E560F13C-3A2D-4A89-D422-61C309A8E9E9",
+		"name": "网事杂谈",
+		"image_list": [{
+			"name": "网事杂谈",
+			"list": [{
+				"url": "../../static/img/index/wszt.png",
+				"name": "网事杂谈",
+			}, {
+				"url": "../../static/img/index/ecy.png",
+				"name": "二次元国家地理",
+			}]
+		}]
 	}, {
-		"url": "../../static/img/index/ecy.png",
-		"name":"网事杂谈",
-	}, {
-		"url": "../../static/img/index/ecy.png",
-		"name":"网事杂谈",
-	}, {
-		"url": "../../static/img/index/ecy.png",
-		"name":"网事杂谈",
-	}],
-	"id": "E560F13C-3A2D-4A89-D422-61C309A8E9E9"
-}, {
-	"article_type": 1,
-	"name":"网事杂谈",
-	"image_list": [{
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}, {
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}, {
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}],
-	"id": "E3263FC6-3719-4B5C-67A9-D3BCA575F8C4"
-}, {
-	"article_type": 2,
-	"name":"网事杂谈",
-	"image_list": [{
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}, {
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}, {
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}],
-	"id": "878CE254-A6BA-43BE-5EB2-7D62A51FFBA6"
-}, {
-	"article_type": 3,
-	"name":"网事杂谈",
-	"image_list": [{
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}, {
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}, {
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}],
-	"id": "8F55895A-329A-40C6-B981-D7715AB47E64"
-}, {
-	"article_type": 4,
-	"name":"网事杂谈",
-	"image_list": [{
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}, {
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}, {
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}],
-	"id": "61B3AB9C-F242-438B-4605-77D55FADF567"
-}, {
-	"article_type": 5,
-	"name":"网事杂谈",
-	"image_list": [{
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}, {
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}, {
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
-		"name":"网事杂谈",
-	}],
-	"id": "E17C426C-A219-43D8-6862-DE5C200E0B2B"
-}, {
-	"article_type": 6,
-	
-	"image_list": [{
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-		"width": 563,
-		"height": 316
-	}],
-	"id": "937F0AA4-AE10-45BE-ABE7-A173DFF05B99"
-}, {
-	"article_type": 7,
-	"image_list": [{
-		"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
-		"width": 563,
-		"height": 316
-	}],
-	"id": "0A8E4195-947B-4B97-E32D-5EC123D0D063"
-}]
+		"article_type": 1,
+		"id": "E3263FC6-3719-4B5C-67A9-D3BCA575F8C4",
+		"name": "艾泽拉斯议事厅",
+		"image_list": [{
+			"name": "艾泽拉斯议事厅",
+			"list": [{
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "艾泽拉斯议事厅",
+			}, {
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "艾泽拉斯风纪委员会",
+			}, {
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "前瞻资讯",
+			}, {
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "经典旧世",
+			}, {
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "国服以外",
+			}, {
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "争霸艾泽拉斯",
+			}]
+		}, {
+			"name": "职业讨论区",
+			"list": [{
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "艾泽拉斯议事厅",
+			}, {
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "艾泽拉斯风纪委员会",
+			}, {
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "前瞻资讯",
+			}, {
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "经典旧世",
+			}, {
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "国服以外",
+			}, {
+				"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+				"name": "争霸艾泽拉斯",
+			}]
+		}]
+	},
+	{
+		"article_type": 2,
+		"id": "878CE254-A6BA-43BE-5EB2-7D62A51FFBA6",
+		"name": "厂商专区",
+		"image_list": [{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			},
+			{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			}
+		]
+	},
+	{
+		"article_type": 3,
+		"id": "8F55895A-329A-40C6-B981-D7715AB47E64",
+		"name": "网事杂谈",
+		"image_list": [{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			},
+			{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			}
+		]
+
+	},
+	{
+		"article_type": 4,
+		"id": "61B3AB9C-F242-438B-4605-77D55FADF567",
+		"name": "网事杂谈",
+		"image_list": [{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			},
+			{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			}
+		]
+
+	},
+	{
+		"article_type": 5,
+		"id": "E17C426C-A219-43D8-6862-DE5C200E0B2B",
+		"name": "网事杂谈",
+		"image_list": [{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			},
+			{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			}
+		]
+	},
+	{
+		"article_type": 6,
+		"id": "937F0AA4-AE10-45BE-ABE7-A173DFF05B99",
+		"name": "网事杂谈",
+		"image_list": [{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			},
+			{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			}
+		]
+
+	},
+	{
+		"article_type": 7,
+		"id": "0A8E4195-947B-4B97-E32D-5EC123D0D063",
+		"name": "网事杂谈",
+		"image_list": [{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			},
+			{
+				"name": "职业讨论区",
+				"list": [{
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}, {
+					"url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+					"name": "网事杂谈",
+				}]
+			}
+		]
+	}
+]
